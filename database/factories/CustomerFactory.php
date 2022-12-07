2978304d5520cf1b5bfce2aa7c34460f734c5e01
Amrinder <keptanamrinder@gmail.com>
2022-12-07 19:35:58 +0000

    Api built

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $type = fake()->randomElement(['I', 'B']);
+        $name = $type == 'I' ? fake()->name() : fake()->company();
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => fake()->email(),
+            'address' => fake()->streetAddress(),
+            'city' => fake()->city(),
+            'state' => fake()->state(),
+            'Postal_code' => fake()->postcode()
+
+        ];
+    }
+}
---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+use App\Models\Invoice;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $status = fake()->randomElement(['B', 'P', 'V']);
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => fake()->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => fake()->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? fake()->dateTimeThisDecade() : NULL
+        ];
+    }
+}
